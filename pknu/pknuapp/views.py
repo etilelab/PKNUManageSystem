3e866f83d5262668d184de73a6f4454fa63fec6d
etilelab <taegyeong7202@gmail.com>
2021-06-08 21:44:23 +0900

    register and login view add

--- pknu/pknuapp/views.py
+++ pknu/pknuapp/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.hashers import make_password, check_password
+from .models import *
+import json
+from django.db.models import Q, Count
+from datetime import datetime, timedelta
+import time
+from operator import itemgetter
+from django.core.paginator import Paginator
+from django.db.models.functions import Cast
+from django.db import connections
 
 # Create your views here.
 
 
 def index(request):
+    # 로그인 여부 확인
+    user_id = request.session.get('user')
+    # row query (쌩 쿼리 select *from XXXXXX)
+    # if user_id:
+    #
+    # else:
+
     res_data = {'test':'test'}
     return render(request, 'index.html', res_data)
 
@@ -12,30 +31,44 @@ def login(request):
     if request.method == 'GET':
         return render(request, 'login.html')
     elif request.method == 'POST':
-        username = request.POST.get('username', None)
+        email = request.POST.get('email', None)
         password = request.POST.get('password', None)
         res_data = {}
-        if not (username and password):
+        if not (email and password):
             res_data['error'] = 'Login failed'
         else:
             try:
-                fcuser = ApdoUsers.objects.get(username=username)
-                if check_password(password, fcuser.password):
-                    request.session['user'] = fcuser.id
+                fcuser = Member.objects.get(email=email)
+                if check_password(password, fcuser.passwd):
+                    request.session['user'] = fcuser.email
                     return redirect('/')
-
                 else:
                     res_data['error'] = 'Login failed'
-            except ApdoUsers.DoesNotExist:
+            except Member.DoesNotExist:
                 res_data['error'] = 'Login failed'
         return render(request, 'login.html', res_data)
 
 
 def logout(request):
-    res_data = {'test':'test'}
-    return render(request, 'index.html', res_data)
+    if request.session.get('user'):
+        del(request.session['user'])
+    return redirect('/')
 
 
 def register(request):
-    res_data = {'test':'test'}
-    return render(request, 'index.html', res_data)
+    if request.method == "GET":
+        return render(request, 'register.html')
+    elif request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+        re_password = request.POST['re_password']
+        employee_id = request.POST['employee_id']
+        res_data = {}
+        if not (email and password and re_password):
+            res_data['error'] = "Input all value"
+        if password != re_password :
+            res_data['error'] = 'Repeat Password '
+        else:
+            user = Member(email=email, passwd=make_password(password), employee_id=employee_id)
+            user.save()
+        return render(request, 'register.html', res_data)
