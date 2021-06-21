5db3ab36baab302aaf1804044755c5d50fd494d2
etilelab <taegyeong7202@gmail.com>
2021-06-21 21:24:48 +0900

    서버 업로드위한

--- pknu/pknu/settings.py
+++ pknu/pknu/settings.py
@@ -28,7 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -112,11 +111,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 TIME_ZONE = 'Asia/Seoul'
 
